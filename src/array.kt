85afe512d2fde13b3277890aad82e052e8082dae
arslon <arslonsaidov300@gmail.com>
2022-06-07 19:58:10 +0500

    adding git ignore

---
+++ src/array.kt
@@ -0,0 +1,53 @@
+fun main() {
+//    val numbers = intArrayOf(1, 2, 3, 4, 5, 6)
+//    println(numbers.joinToString(" "))
+//    val characters = charArrayOf('2', '3', 'd', 'f')
+//    println(characters.joinToString())
+//    val doubles = doubleArrayOf(1.2, 3.4, 5.4, 5.5)
+//    println(doubles.joinToString())
+//    ------------------------------------------------------
+//
+//    val numbers = IntArray(5)
+//    println(numbers.joinToString() )
+
+//    val doubles = DoubleArray(7) // an array of 7 doubles
+//    println(doubles.joinToString())
+////    -------------------------------------------------------
+//    val numbers = IntArray(5){ readln().toInt() }
+//    println(numbers.joinToString())
+//    ---------------------------------------------------------
+//    val numbers = readln().split(" ").map { it.toInt() }.toTypedArray()
+//    println(numbers.joinToString())
+//    -----------------------------------------------------------
+//    val regex = "\\s+".toRegex()// to ignore spaces and tabs via regex
+//    val str = "1 2\t\t3  4\t5  6"
+//    val nums = str.split(regex).map { it.toInt() }.toTypedArray()
+//    println(nums.joinToString()) // 1, 2, 3, 4, 5, 6
+//------------------------------------------------------------ size array
+//    val numberArray = intArrayOf(1, 2, 3, 4, 5, 6)
+//    println(numberArray.size)
+//------------------------------------------------------------- accessing elements
+
+//    val numbers = IntArray(5)
+//    numbers[0] = 1
+//    numbers[1] = 2
+//    numbers[2] = 3
+//    numbers[3] = 4
+//    numbers[4] = 5
+//    for (x in 0..numbers.size - 1) {
+//        println(numbers[x])
+//    }
+//    println(numbers.first())
+//    println(numbers.last())
+//    println(numbers.lastIndex)
+//    println(numbers.get(numbers.lastIndex))
+//    -----------------------------------------------------------------------------
+//    compare arrays
+//    val numbers1 = intArrayOf(1, 2, 3, 4)
+//    val numbers2 = intArrayOf(1, 2, 3, 4)
+//    val numbers3 = intArrayOf(1, 2, 3)
+//    println(numbers1.contentEquals(numbers2))
+//    println(numbers1.contentEquals(numbers3))
+
+
+}
---
+++ src/stringArray.kt
@@ -0,0 +1,31 @@
+fun main() {
+//    var stringArray = arrayOf("array","of","string")
+//    var strictIntArray = arrayOf<Int>(1, 2, 3, 4, 5)
+// ------------------------------------------------------
+//    val emptyArray : Array<Int> = emptyArray()
+//    println(emptyArray.size)    // 0
+
+//    val emptyArray:Array<Int> = arrayOf()
+//-------------------------------------------------------------
+//     adding two arrays
+//    val southernCross = arrayOf("Acrux", "Gacrux", "Imai", "Mimosa")
+//    val stars = arrayOf("Ginan", "Mu Crucis")
+//    val newArray = southernCross + stars
+//    println(newArray.joinToString())    //  Acrux, Gacrux, Imai, Mimosa, Ginan, Mu Crucis
+////    -----------------------------------------------------------------------------
+//    var southernCross = emptyArray<String>()
+//    southernCross += "Acrux"
+//    southernCross += "Gacrux"
+//    southernCross += "Imai"
+//    println(southernCross.joinToString())   // Acrux, Gacrux, Imai
+//------------------------------------------------------------
+//    var southernCross = arrayOf("Acrux", "Gacrux", "Imai")
+//    southernCross += "Mimosa"
+//    println(southernCross.joinToString())  //  Acrux, Gacrux, Imai, Mimosa
+// ---------------------------------------------------------------------
+//    we cannot add element if array is declared as val
+//    val southernCross = arrayOf("Acrux", "Gacrux", "Imai", "Mimosa")
+//    southernCross += "Ginan" // will not compile
+
+
+}
