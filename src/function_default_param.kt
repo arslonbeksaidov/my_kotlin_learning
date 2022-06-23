83af8e55a36fbfe3d488b8e827aed2ef2c8c6a84
arslon <a4317990a>
2022-06-23 20:45:17 +0500

    function named and params

---
+++ src/function_default_param.kt
@@ -0,0 +1,21 @@
+fun main() {
+//    fun printLine(line: String = "", end: String = "\n") = print("$line$end")
+//    printLine(line = "arslon saidov", end = " 111")
+//    printLine("arslon","saidov")
+
+//    fun findMax(n1: Int, n2: Int, absolute: Boolean = false): Int {
+//        val v1: Int
+//        val v2: Int
+//        if (absolute) {
+//            v1 = Math.abs(n1)
+//            v2 = Math.abs(n2)
+//        } else {
+//            v1 = n1
+//            v2 = n2
+//        }
+//        return if (v1 > v2) n1 else n2
+//    }
+//    val ans = findMax(4,5,false)
+//    print(ans)
+
+}
---
+++ src/function_named_param.kt
@@ -0,0 +1,25 @@
+fun main() {
+//    fun calcEndDayAmount(startAmount: Int, ticketPrice: Int, soldTickets: Int) =
+//        startAmount + ticketPrice * soldTickets
+//    calcEndDayAmount(startAmount = 1000, 10, 500)
+//
+//    fun sum2(a: Int, b: Int = a) = a + b
+//
+//    sum2(1)    // 1 + 1
+//    sum2(2, 3) // 2 + 3
+//
+//
+
+
+//    fun sumBank(amount: Int = 1000, percent: Int = 5, years: Int = 10): Int =
+//        (amount.toDouble() * Math.pow(1 + percent.toDouble() / 100.00, years.toDouble())).toInt()
+//
+//    val typeChanged = readln()
+//    val value = readln().toInt()
+//
+//    when (typeChanged) {
+//        "amount" -> println(sumBank(amount = value))
+//        "percent" -> println(sumBank(percent = value))
+//        "years" -> println(sumBank(years = value))
+//    }
+}
