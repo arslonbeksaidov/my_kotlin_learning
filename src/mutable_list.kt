ac175bc4acea65e10e2a359f36bf95c99d34333b
arslon <a4317990a>
2022-06-25 16:35:57 +0500

    list immutable

---
+++ src/mutable_list.kt
@@ -0,0 +1,51 @@
+fun main() {
+//    var places = listOf<String>("Paris", "Moscow", "Tokyo")
+//    println(places) // output: [Paris, Moscow, Tokyo]
+//    places += "Tashkent"
+//    print(places)
+//
+
+//    val places = mutableListOf<String>("Paris","Moscow","Tokyo")
+//    places.add("Tashkent")
+//    print(places)
+
+//    list to mutable list
+//    val cars = listOf("Ford", "Toyota").toMutableList()
+//    cars.add("Tesla")
+//    println(cars) // output: [Ford, Toyota, Tesla]
+
+//    val products = listOf<String>("Milk","Cheeze","Coke")
+//    val finalList = products.toMutableList()
+//    finalList.add("Chips")
+//    finalList[0] = "Water"
+//    print(finalList)
+
+
+//    val products = mutableListOf<String>("Milk","Cheeze")
+//    val dadsProducts = listOf<String>("Banana","Watermelon","Apple")
+//    products.addAll(dadsProducts)
+//    println(products)
+//    products.removeAt(0)
+//    println(products)
+//
+//    products.remove("Banana")
+//    println(products)
+//
+//    products.clear()
+//    println(products  )
+
+//    fun solution(numbers: List<Int>, number: Int): MutableList<Int> {
+//        val numbers_list = readln().split(" ").map { it.toInt() }.toMutableList()
+//        val num = readln().toInt()
+//        numbers_list.add(num)
+//        return numbers_list
+//    }
+//    print(solution(listOf(1,2,3),5))
+
+
+
+//    fun solution(strings: MutableList<String>, str: String): MutableList<String> =
+//        strings.map {
+//            if (it == str) "Banana" else it
+//        }.toMutableList()``
+}
